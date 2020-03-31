c1fc258619d85d4d731fc77918701b54463c6ab2
Ben Honken <honkenb@gmail.com>
2020-03-30 23:11:41 -0500

    added necessary functions and set layout.

--- src/components/Direction.js
+++ src/components/Direction.js
@@ -1,21 +1,42 @@
 import React from "react";
 
-function EmployeeInfo(props) {
+function Direction(props) {
+  let northSouth = false;
+  if(props.directionList && props.directionList[0]){
+      if (props.directionList[0].Value === "4"){
+          northSouth = true
+      }
+  }
   return(
   <div>
     <div className="card text-center">
       <div className="card-header" style={{height: 110}}>
-        <h2>Order:</h2>
+        <h2>Direction:</h2>
       </div>
       <div className="card-body"></div>
-      <form style={{height: 208}}>
-        <input onChange={props.handleInputChange} type="radio" id="1" name="sortType" value="1" />
-        <label for="name.first">Ascending</label><br />
-        <input onChange={props.handleInputChange} type="radio" id="-1" name="sortType" value="-1" />
-        <label for="name.first">Decending</label><br /><br /><br />
-      </form>
+        { props.directionList.length>0 ? ( 
+        <div>
+          {northSouth ? (
+            <form style={{height: 208}}>
+              <input onChange={props.handleInputChange} onClick={props.getStopList} type="radio" id="north" name="direction" value="4" />
+              <label for="direction">North</label><br />
+              <input onChange={props.handleInputChange} type="radio" id="south" name="direction" value="1" />
+              <label for="direction">South</label><br /><br /><br />
+            </form>
+          ):(
+            <form style={{height: 208}}>
+              <input onChange={props.handleInputChange} type="radio" id="east" name="direction" value="2" />
+              <label for="direction">East</label><br />
+              <input onChange={props.handleInputChange} type="radio" id="west" name="direction" value="3" />
+              <label for="direction">West</label><br /><br /><br />
+            </form>
+          )}
+        </div>
+        ):(
+          <div>Please choose a route</div>
+        )}
     </div>
   </div>)
 }
 
-export default EmployeeInfo;
+export default Direction;
--- src/components/NextBusContainer.js
+++ src/components/NextBusContainer.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import Container from "./Container";
 import Row from "./Row";
 import Col from "./Col";
 import SearchFormRoute from "./SearchFormRoute";
 import SearchFormStop from "./SearchFormStop";
+import RouteSearchSuggest from "./RouteSearchSuggest";
+import StopSearchSuggest from "./StopSearchSuggest";
 import Direction from "./Direction";
+import Result from "./Result";
 const axios = require("axios");
 
 class NextBusContainer extends Component {
@@ -22,13 +25,48 @@ class NextBusContainer extends Component {
   };
   
   componentDidMount() {
-    this.getRoutes;
+    this.getRoutes();
   }
-  getRoutes() {
+  getRoutes = async () => {
     let response = await axios.get('https://svc.metrotransit.org/NexTrip/Routes?format=json')
     this.setState({ 'routeList': response.data });
   }
-
+  getRouteNumber = async (event) => {
+    event.preventDefault();
+    let goodRoute = false
+    for (let i=0; i<this.state.routeList.length; i++){
+      if (this.state.routeList[i].Description.includes(this.state.route) && !goodRoute){
+          this.setState({ 'routeNumber': this.state.routeList[i].Route});
+          goodRoute = true;
+      }
+    }
+    this.getDirectionList();
+  }
+  getDirectionList = async () => {
+    let response = await axios.get('https://svc.metrotransit.org/NexTrip/Directions/' + this.state.routeNumber + '?format=json')
+    this.setState({ 'directionList': response.data });
+  }
+  clearDirection() {
+    this.setState({ 'direction': 0 });
+  }
+  getStopList = async () => {
+    let response = await axios.get('https://svc.metrotransit.org/NexTrip/Stops/' + this.state.routeNumber + '/' + this.state.direction + '?format=json')
+    this.setState({ 'stopList': response.data });
+  }
+  getStopValue = async (event) => {
+    event.preventDefault();
+    let goodStop = false
+    for (let i=0; i<this.state.stopList.length; i++){
+      if (this.state.stopList[i].Text === this.state.stop){
+        this.setState({ 'stopValue': this.state.stopList[i].Route});
+        goodStop = true;
+      }
+    }
+    this.getDepartureList();
+  }
+  clearStop() {
+    this.setState({ 'stop': "" });
+  }
   handleInputChange = event => {
     const value = event.target.value;
     const name = event.target.name;
@@ -36,24 +74,64 @@ class NextBusContainer extends Component {
       [name]: value
     });
   };
-  getDeparture() {
+  getDepartureList = async () => {
     let response = await axios.get('https://svc.metrotransit.org/NexTrip/' + this.state.routeNumber + '/' + this.state.direction + '/' + this.state.stopValue + '?format=json')
     this.setState({ 'departureList': response.data });
+    console.log(this.state.departureList);
+  }
+  handleClickRoute = (event) => {
+    event.preventDefault();
+    this.setState({ 'route': event.currentTarget.dataset.value });
+  }
+  handleClickStop = (event) => {
+    event.preventDefault();
+    this.setState({ 'stop': event.currentTarget.dataset.value });
   }
   handleFormSubmit = event => {
     event.preventDefault();
     this.getDeparture();
   };
   render() {
+    let route=this.state.route;
+    let direction=this.state.direction
+    let stop=this.state.stop
+    let stopValue=this.state.stopValue
+    let departureList=this.state.departureList
+    // useEffect(() => {
+    //   this.getRouteNumber();
+    // },[route])
+    // useEffect(() => {
+    //   this.clearDirection();
+    // },[route])
+    // useEffect(() => {
+    //   this.getDirectionList();
+    // },[route])
+    // useEffect(() => {
+    //   this.clearStop();
+    // },[route])
+    // useEffect(() => {
+    //   this.getStopList();
+    // },[direction])
+    // useEffect(() => {
+    //   this.getStopValue();
+    // },[stop])
+    // useEffect(() => {
+    //   this.getDepartureList();
+    // },[stopValue])
+    // useEffect(() => {
+    //   this.handleFormSubmit();
+    // },[departureList])
     return (
       <Container>
         <Row>
         <Col size="md-4">
-            <SearchFormRoute
+            <RouteSearchSuggest
               routeList={this.state.routeList}
               route={this.state.route}
               routeNumber={this.state.routeNumber}
               handleInputChange={this.handleInputChange}
+              handleClickRoute={this.handleClickRoute}
+              getRouteNumber={this.getRouteNumber}
             />
           </Col>
           <Col size="md-4">
@@ -61,14 +139,17 @@ class NextBusContainer extends Component {
               direction={this.state.direction}
               directionList={this.state.directionList}
               handleInputChange={this.handleInputChange}
+              getStopList={this.getStopList}
             />
           </Col>
           <Col size="md-4">
-            <SearchFormStop
+          <StopSearchSuggest
               stopList={this.state.stopList}
               stop={this.state.stop}
               stopValue={this.state.stopValue}
               handleInputChange={this.handleInputChange}
+              handleClickStop={this.handleClickStop}
+              getStopValue={this.getStopValue}
             />
           </Col>
         </Row>
