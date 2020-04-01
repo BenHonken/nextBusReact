843eb8d615ff4d4a16dc2339ecebaf1738450219
Ben Honken <honkenb@gmail.com>
2020-03-31 23:11:11 -0500

    added conditional to inform user if all departures have left for the day

--- src/components/Result.js
+++ src/components/Result.js
@@ -8,12 +8,15 @@ function Result(props) {
   else if (props.departureList[0]){
     departureText="Next Departure: " + props.departureList[0].DepartureText;
   }
+  else 
   return (
     <div className="text-center">
       {props.departureList && props.departureList.length > 0 ? (
         <h2>{departureText}</h2>
       ) : (
-          <h2>Please enter a route, direction, and stop!</h2>
+        <div>
+          {props.stopValue && props.stopValue.length > 0 ? ((<h2>No departures left today!</h2>)):(<h2>Please enter a route, direction, and stop!</h2>)}
+        </div>          
         )}
     </div>
   );
